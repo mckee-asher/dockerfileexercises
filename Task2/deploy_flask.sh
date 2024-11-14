b433216d3e1d744939585942a2a93d0bf5e3cab3
Asher McKee <shannenlmckee@gmail.com>
2024-11-14 16:53:53 +0000

    reverted rename of images

--- Task2/deploy_flask.sh
+++ Task2/deploy_flask.sh
@@ -4,6 +4,6 @@ docker build -t trio-task-flask-app:latest flask-app
 # run flask container
 docker run -d \
     -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
-    --name amckee457/flask-app \
+    --name flask-app \
     --network trio-task-network \
     trio-task-flask-app:latest
--- Task2/deploy_mysql.sh
+++ Task2/deploy_mysql.sh
@@ -4,6 +4,6 @@ docker build -t trio-task-mysql:5.7 db
 # run mysql container
 docker run -d \
     -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
-    --name amckee457/mysql \
+    --name mysql \
     --network trio-task-network \
     trio-task-mysql:5.7
