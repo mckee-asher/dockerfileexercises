7569a1c3f32a644d97ee36125f92a6e586ea876f
Asher McKee <shannenlmckee@gmail.com>
2024-11-14 17:25:58 +0000

    Altered jenkinsfile and deploy.sh

--- Task2/deploy_flask.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# build flask
-docker build -t trio-task-flask-app:latest flask-app
-# run flask container
-docker run -d \
-    -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
-    --name flask-app \
-    --network trio-task-network \
-    trio-task-flask-app:latest
--- Task2/deploy_mysql.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# build mysql
-docker build -t trio-task-mysql:5.7 db
-# run mysql container
-docker run -d \
-    -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
-    --name mysql \
-    --network trio-task-network \
-    trio-task-mysql:5.7
