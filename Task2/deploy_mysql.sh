b1c384c339b0cbd57a90298cab6ca85441b1e3e7
Asher McKee <shannenlmckee@gmail.com>
2024-11-14 16:52:36 +0000

    renamed images

--- Task2/deploy_mysql.sh
+++ Task2/deploy_mysql.sh
@@ -4,6 +4,6 @@ docker build -t trio-task-mysql:5.7 db
 # run mysql container
 docker run -d \
     -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
-    --name mysql \
+    --name amckee457/mysql \
     --network trio-task-network \
     trio-task-mysql:5.7
